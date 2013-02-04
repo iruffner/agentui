fb94a067a181d30a584931771b39cb89b8f84c22
Isaiah Ruffner <isaiah@model3.net>
2013-02-04 08:02:39 -0500

    new stuff

---
+++ client/haxe_src/ui/util/FixedSizeArray.hx
@@ -0,0 +1,26 @@
+package ui.util;
+
+using ui.helper.ArrayHelper;
+
+class FixedSizeArray<T> {
+
+	private var _delegate: Array<T>;
+	private var _maxSize: Int;
+
+	public function new(maxSize: Int) {
+		_maxSize = maxSize;
+		_delegate = new Array<T>();
+	}
+
+	public function push(t: T): Void {
+		if(_delegate.length >= _maxSize) {
+			_delegate.shift();
+		}
+		_delegate.push(t);
+	}
+
+	public function contains(t: T): Bool {
+		return _delegate.contains(t);
+	}
+	
+}
