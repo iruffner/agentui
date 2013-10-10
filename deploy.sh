42a2635fb5e049d00bab20620db4bfd4e6b3a4a8
Glen Marchesani <glen@model3.net>
2013-10-10 10:50:10 -0600

    deploy script has server var

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,11 @@
 
-rsync --delete --compress --archive --partial --progress --stats client/ root@64.27.3.17:/opt/agentui/webapp/
-rsync --compress --archive --partial --progress --stats proxy/src/main/webapp/ root@64.27.3.17:/opt/agentui/webapp/
+# calpop
+#SERVER=root@64.27.3.17:/opt/agentui/webapp/
+
+# agentui-dev
+SERVER=ubuntu@ec2-54-212-15-76.us-west-2.compute.amazonaws.com:/opt/agentui/webapp/
+
+
+rsync --delete --compress --archive --partial --progress --stats client/ ${SERVER}
+rsync --compress --archive --partial --progress --stats proxy/src/main/webapp/ ${SERVER}
 
