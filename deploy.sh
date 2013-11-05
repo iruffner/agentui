d42d9f433961f722ab8a70941959c78b07e602b8
Isaiah Ruffner <isaiah@model3.net>
2013-11-05 12:27:26 -0500

    disabled all servers to prevent accidental deploy

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 #SERVER=root@64.27.3.17:/opt/agentui/webapp/
 
 # agentui-dev
-SERVER=ubuntu@ec2-54-212-15-76.us-west-2.compute.amazonaws.com:/opt/agentui/webapp/
+#SERVER=ubuntu@ec2-54-212-15-76.us-west-2.compute.amazonaws.com:/opt/agentui/webapp/
 
 # agentui-test
 #SERVER=ubuntu@ec2-54-214-229-124.us-west-2.compute.amazonaws.com:/opt/agentui/webapp/
