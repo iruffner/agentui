6fd099e61e26fa0dcf237aee2c5fdeebdd85e020
Isaiah Ruffner <isaiah@model3.net>
2013-11-25 19:35:18 -0500

    modded during deployment

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@
 #SERVER=ubuntu@ec2-54-212-15-76.us-west-2.compute.amazonaws.com:/opt/agentui/webapp/client/
 
 # agentui-test
-#SERVER=ubuntu@ec2-54-214-229-124.us-west-2.compute.amazonaws.com:/opt/agentui/webapp/client/
+SERVER=ubuntu@ec2-54-214-229-124.us-west-2.compute.amazonaws.com:/opt/agentui/webapp/client/
 
 
 rsync --delete --compress --links --recursive --partial --progress --stats client/ ${SERVER}
