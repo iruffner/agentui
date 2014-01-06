40093aec289d8d7dc61ee52fea049a9dd988ca3b
Peter R. G. Small <prgsmall@gmail.com>
2014-01-06 14:31:12 -0700

    Put all of the elements in the time marker into a gropu

--- client/haxe/src/ui/widget/score/TimeMarker.hx
+++ client/haxe/src/ui/widget/score/TimeMarker.hx
@@ -7,10 +7,14 @@ class TimeMarker
 	private var paper: Snap;
 	private var width: Float;
 	private var line:SnapElement;
+	private var group:SnapElement;
 
 	public function new(paper:Snap, width:Float) {
 		this.paper = paper;
 		this.width = width;
+		this.group = paper.group(paper, [])
+		                  .attr("id", "time-marker");
+
 
 		drawTimeLine();
 	}
@@ -25,35 +29,36 @@ class TimeMarker
 		};
 
 	  	this.line = paper.line(margin, y, width - margin, y).attr(attrs);
+	  	this.group.append(this.line);
 
 	  	var interval:Float = (width - 2*margin) / 24;
 		var x:Float = margin;
 	  	for(i in 0...25) {
 	  		switch(i) {
 	  			case 0, 12, 24: 
-	  				paper.line(x, y-margin, x, y+margin).attr(attrs);
+	  				this.group.append(paper.line(x, y-margin, x, y+margin).attr(attrs));
 	  				if(i == 0)
-			  			paper.text(x+3, y - margin + 1, "2013").attr({fontSize: "8px"});
+			  			this.group.append(paper.text(x+3, y - margin + 1, "2013").attr({fontSize: "8px"}));
 		  			else if(i == 12)
-		  				paper.text(x+3, y - margin + 1, "2012").attr({fontSize: "8px"});
+		  				this.group.append(paper.text(x+3, y - margin + 1, "2012").attr({fontSize: "8px"}));
 			  		if(i == 0 || i == 12)
-			  			paper.text(x+10, y + margin, "Dec").transform("rotate(30," + x + "," + y + ")").attr({fontSize: "8px"});
+			  			this.group.append(paper.text(x+10, y + margin, "Dec").transform("rotate(30," + x + "," + y + ")").attr({fontSize: "8px"}));
 			  		x += interval;
 		  		case 3, 15: 
-	  				paper.line(x, y, x, y+margin+2).attr(attrs);
-			  		paper.text(x+10, y + margin, "Sep").transform("rotate(30," + x + "," + y + ")").attr({fontSize: "8px"});
+	  				this.group.append(paper.line(x, y, x, y+margin+2).attr(attrs));
+			  		this.group.append(paper.text(x+10, y + margin, "Sep").transform("rotate(30," + x + "," + y + ")").attr({fontSize: "8px"}));
 			  		x += interval;
 			  	case 6, 18: 
-	  				paper.line(x, y, x, y+margin+2).attr(attrs);
-			  		paper.text(x+10, y + margin, "Jun").transform("rotate(30," + x + "," + y + ")").attr({fontSize: "8px"});
+	  				this.group.append(paper.line(x, y, x, y+margin+2).attr(attrs));
+			  		this.group.append(paper.text(x+10, y + margin, "Jun").transform("rotate(30," + x + "," + y + ")").attr({fontSize: "8px"}));
 			  		x += interval;
 			  	case 9, 21: 
-	  				paper.line(x, y, x, y+margin+2).attr(attrs);
-			  		paper.text(x+10, y + margin, "Mar").transform("rotate(30," + x + "," + y + ")").attr({fontSize: "8px"});
+	  				this.group.append(paper.line(x, y, x, y+margin+2).attr(attrs));
+			  		this.group.append(paper.text(x+10, y + margin, "Mar").transform("rotate(30," + x + "," + y + ")").attr({fontSize: "8px"}));
 			  		x += interval;
 
 	  			case _: 
-			  		paper.line(x, y, x, y+margin).attr(attrs);
+			  		this.group.append(paper.line(x, y, x, y+margin).attr(attrs));
 			  		x += interval;
 	  		}
 	  	}
